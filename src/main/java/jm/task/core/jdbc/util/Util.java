57a38c3fcebdbd9b9e704468e990a641abb7346d
DanikWRT <danil23004@gmail.com>
2023-10-17 00:04:25 +0700

    Util (connect to base)

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,21 +1,17 @@
 package jm.task.core.jdbc.util;
-
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/kata_114";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
-    public static void main(String[] args) {
-        Connection connection;
+    public static Connection getConnection() {
         try {
-            Driver driver = new FabricM
-
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            return connection;
         } catch (SQLException e) {
-            System.err.println("Не удалось загрузить класс драйвера");
+            throw new RuntimeException(e);
         }
     }
 }
