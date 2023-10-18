4c6f732eb21e4646e16cd5875cf8d624962257b7
DanikWRT <danil23004@gmail.com>
2023-10-18 22:07:01 +0700

    release: main + UserServiceImpl

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
-    }
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+
+        userService.saveUser("Name1", "lastName1", (byte) 20);
+
+        userService.removeUserById(1);
+        List<User> users = userService.getAllUsers();
+        for (User list: users) {
+            System.out.println(list);
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+     }
 }
